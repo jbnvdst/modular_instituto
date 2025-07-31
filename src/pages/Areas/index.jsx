253e955aa6dec7a16737c124eaad602e62275bbe
Gabriel Sanchez <gisgarci99@gmail.com>
2025-07-31 15:45:23 -0600

    Showing area tasks to debug in Akary's workspace

--- src/pages/Areas/index.jsx
+++ src/pages/Areas/index.jsx
@@ -244,7 +244,7 @@ function Areas() {
                 <div className='grid grid-cols-[70%_1fr] gap-6'>
                   <div className="bg-white rounded-lg shadow-sm p-6">
                     <div className="flex justify-between items-center mb-4">
-                        <h3 className="text-lg font-semibold text-gray-900">Tareas del Área</h3>
+                        <h3 onClick={() => console.log(areaData.tasks)} className="text-lg font-semibold text-gray-900">Tareas del Área</h3>
                     </div>
                     <div className="mt-4 block text-sm text-gray-700">
                           {resolved ? (
