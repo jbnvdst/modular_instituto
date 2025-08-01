487af0350581603c86c3ba7027934142ca31219b
Gabriel Sanchez <gisgarci99@gmail.com>
2025-08-01 13:15:53 -0600

    Creating useEffect for fetching Notes

--- src/utils/context/AreasContext/index.jsx
+++ src/utils/context/AreasContext/index.jsx
@@ -13,6 +13,7 @@ export const AreasProvider = ({ children }) => {
     try {
       const response = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/area/tasksByArea`);
       setAreas(response.data);
+      console.log("Areas fetched:", response.data);
     } catch (error) {
       console.error("Error fetching areas:", error);
     }
